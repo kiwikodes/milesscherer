0d1e4152a04dfe94b5b588ed6462dac89d530959
pleobyte <pleobyte@gmail.com>
2021-06-28 18:52:47 +0700

    Create blank pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      
-    </div>
-  )
+  	return (
+    	<div className={styles.container}>
+    	</div>
+  	)
 }
