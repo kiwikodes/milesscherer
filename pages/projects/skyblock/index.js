c96f56a1ea4960a96f2819817e1559c5c8ad249d
pleobyte <pleobyte@gmail.com>
2021-06-28 20:04:57 +0700

    Fix #4

--- pages/projects/skyblock/index.js
+++ pages/projects/skyblock/index.js
@@ -3,6 +3,12 @@ import styles from './styles.module.css';
 export default function Skyblock() {
     return (
         <div className={styles.container}>
+            <style jsx global>{`
+                body {
+                    margin: 0px;
+                    padding: 0px;
+                }
+            `}</style>
             <table className={styles.table}>
                 <tr>
                     <th>Firstname</th>
